97a0894cc9b1efa11e7ee850981c4ff2e23e7818
Derick707 <derick.silva18@escola.pr.gov.br>
2026-03-12 11:41:31 -0300

    js

---
+++ funcoes.js
@@ -0,0 +1,12 @@
+//let x = "";
+//console.log (x);
+//x = "oi";
+
+//DECLARAÇÃO DE FUNÇÃO
+
+// 1) declara a função
+// 2) valor de soma()
+
+function imprimeTexto(texto){
+    console.log(texto)
+}
